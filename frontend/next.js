d1efef03f41af6b29dc439c1343fddd02e7c7a4c
K-wadi <302652872@student.rocmondriaan.nl>
2025-02-09 01:45:00 +0100

    Re-adding project files

--- frontend/next.js
+++
@@ -1,5 +0,0 @@
-fetch(`${process.env.REACT_APP_API_URL}/endpoint`)
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
-  console.log(process.env.REACT_APP_API_URL);
